3a682d3d180d9abf2ea790475fcd42de4dd9b14b
8848taihejin <2101355157@qq.com>
2024-04-19 23:28:55 +0800

    updata

--- exercises/generics/generics2.rs
+++ exercises/generics/generics2.rs
@@ -6,15 +6,18 @@
 // Execute `rustlings hint generics2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 struct Wrapper {
     value: u32,
+    str_value: String,
 }
 
 impl Wrapper {
     pub fn new(value: u32) -> Self {
-        Wrapper { value }
+        Wrapper { value, str_value: String::new() }
+    }
+    pub fn new_str(str_value: &str) -> Self {
+        Wrapper {value: 0,str_value: str_value.to_string() }
     }
 }
 
@@ -29,6 +32,6 @@ mod tests {
 
     #[test]
     fn store_str_in_wrapper() {
-        assert_eq!(Wrapper::new("Foo").value, "Foo");
+        assert_eq!(Wrapper::new_str("Foo").str_value, "Foo");
     }
 }
